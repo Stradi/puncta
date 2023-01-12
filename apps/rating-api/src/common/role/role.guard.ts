4cf988121f88c8348e1499221546ec2995c66b83
Batın Evirgen <batinevirgen@gmail.com>
2023-01-12 16:56:12 +0300

    feat(api): add Guard and Decorators for RBAC

---
+++ apps/rating-api/src/common/role/role.guard.ts
@@ -0,0 +1,21 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { ROLES_KEY } from './roles.decorator';
+import { Role } from './roles.enum';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const ctx = GqlExecutionContext.create(context);
+
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [ctx.getHandler(), ctx.getClass()]);
+    if (!requiredRoles) {
+      return true;
+    }
+    const { user } = ctx.getContext().req;
+    return requiredRoles.some((role) => user.role === role);
+  }
+}
