1dfffad5b86214e0d9a07409d82b697fe23e9b11
Batın Evirgen <batinevirgen@gmail.com>
2023-01-18 19:13:54 +0300

    fix(web): add primary brand color to tailwind config as custom color

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#e6fffe",
+          "light-hover": "#d9fffd",
+          "light-active": "#b0fffa",
+          normal: "#00fff0",
+          "normal-hover": "#00e6d8",
+          "normal-active": "#00ccc0",
+          dark: "#00bfb4",
+          "dark-hover": "#009990",
+          "dark-active": "#00736c",
+          darker: "#005954",
+        },
+      },
+    },
   },
   plugins: [],
 };
