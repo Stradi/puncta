0575efe6790d0f367bb361b4a2d4481cf3cbde41
Batın Evirgen <batinevirgen@gmail.com>
2023-01-21 15:08:59 +0300

    fix(web): use Formik to handle login page

--- apps/web/src/app/giris-yap/page.tsx
+++ apps/web/src/app/giris-yap/page.tsx
@@ -1,65 +1,176 @@
 "use client";
 
-import Button from "@/components/Button";
-import TextInput from "@/components/TextInput";
 import { AuthContext } from "@/context/AuthContext";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
+import Button from "@/components/Button";
+import { Card } from "@/components/Card";
+import { cn } from "@/lib/utils";
+import { useFormik } from "formik";
+import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 
-//TODO: We should probably use a form library such as Formik.
+//TODO: We should redirect the user to the profile page after
+// successfull login. Lines: 25,59
 export default function Page() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  type FormValues = {
+    username: string;
+    password: string;
+  };
 
   const authContext = useContext(AuthContext);
   const router = useRouter();
 
-  async function onFormSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const result = await authContext.login(username, password);
-
-    if (result) {
-      router.push("/");
-    } else {
-      alert("Kullanıcı adı veya şifre yanlış!");
+  const validate = (values: FormValues) => {
+    const errors = {} as FormValues;
+    if (!values.username) {
+      errors.username = "Kullanıcı adı boş bırakılamaz.";
+    } else if (values.username.length < 3) {
+      errors.username = "Kullanıcı adı en az 3 karakter olmalıdır.";
+    } else if (values.username.length > 20) {
+      errors.username = "Kullanıcı adı en fazla 20 karakter olmalıdır.";
     }
-  }
+
+    if (!values.password) {
+      errors.password = "Şifre boş bırakılamaz.";
+    } else if (values.password.length < 8) {
+      errors.password = "Şifre en az 6 karakter olmalıdır.";
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: async (values) => {
+      const response = await authContext.login(
+        values.username,
+        values.password
+      );
+      if (response) {
+        router.push("/");
+      } else {
+        formik.errors.username = "Kullanıcı adı hatalı.";
+        formik.errors.password = "Şifre hatalı.";
+      }
+    },
+    validate,
+  });
 
   return (
-    <div className="mx-auto my-16 max-w-lg space-y-4 border border-black p-4">
+    <Card className="mx-auto my-16 max-w-lg p-4 pt-16 pb-8">
       <h1 className="text-center text-3xl font-bold">Giriş Yap</h1>
-      <form className="space-y-4" onSubmit={onFormSubmit}>
-        <TextInput
-          label="Kullanıcı Adı"
-          placeholder="Kullanıcı adın"
-          type="text"
-          required
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <TextInput
-          label="Şifre"
-          placeholder="Şifren"
-          type="password"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button fullWidth>Giriş Yap</Button>
+      <form
+        onSubmit={formik.handleSubmit}
+        className={cn(
+          "flex flex-col space-y-8",
+          "[&>*]:flex [&>*]:flex-col",
+
+          // Label
+          "[&_label]:font-medium [&_label]:text-neutral-700",
+
+          // Input element
+          "[&_input]:rounded-full [&_input]:px-4 [&_input]:py-2 [&_input]:transition [&_input]:duration-100",
+          "[&_input]:border [&_input]:border-black [&_input]:font-medium [&_input]:outline-none",
+
+          // Error message
+          "[&_input~div]:text-sm [&_input~div]:font-medium [&_input~div]:text-red-600"
+        )}
+      >
+        <div>
+          <label htmlFor="username">Kullanıcı Adı</label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.username}
+            className={cn(
+              "hover:ring-2 hover:ring-black",
+              "focus:ring-2 focus:ring-black"
+            )}
+          />
+          <AnimatePresence>
+            {formik.errors.username && formik.touched.username && (
+              <motion.div
+                initial={{
+                  height: 0,
+                  opacity: 0,
+                }}
+                animate={{
+                  height: "auto",
+                  opacity: 1,
+                }}
+                exit={{
+                  height: 0,
+                  opacity: 0,
+                }}
+              >
+                <p className="mt-2">{formik.errors.username}</p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+
+        <div>
+          <label htmlFor="password">Şifre</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            className={cn(
+              "hover:ring-2 hover:ring-black",
+              "focus:ring-2 focus:ring-black"
+            )}
+          />
+          <AnimatePresence>
+            {formik.errors.password && formik.touched.password && (
+              <motion.div
+                initial={{
+                  height: 0,
+                  opacity: 0,
+                }}
+                animate={{
+                  height: "auto",
+                  opacity: 1,
+                }}
+                exit={{
+                  height: 0,
+                  opacity: 0,
+                }}
+              >
+                <p className="mt-2">{formik.errors.password}</p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+
+        <div>
+          <Button
+            variant="primary"
+            fullWidth
+            disabled={
+              formik.errors.username || formik.errors.password ? true : false
+            }
+          >
+            Giriş Yap
+          </Button>
+          <Button variant="text">Şifreni mi unuttun?</Button>
+        </div>
+
+        <div className="h-px w-full bg-black"></div>
+        <div className="flex w-full items-center justify-center">
+          <span className="font-medium">Hesabın yok mu?</span>
+          <Button variant="text" asLink href="/kayit-ol" className="p-2 py-0">
+            Kayıt ol
+          </Button>
+        </div>
       </form>
-      <div className="h-px w-full bg-black"></div>
-      <div className="flex justify-center">
-        <Button variant="text" className="py-0">
-          Şifreni mi unutttun?
-        </Button>
-      </div>
-      <div className="flex w-full items-center justify-center">
-        <span className="font-medium">Hesabın yok mu?</span>
-        <Button variant="text" className="p-2 py-0">
-          Kayıt ol
-        </Button>
-      </div>
-    </div>
+    </Card>
   );
 }
