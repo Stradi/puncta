0623b6393a229674c8b302767eeff06764726c10
Batın Evirgen <batinevirgen@gmail.com>
2023-01-24 13:20:59 +0300

    fix(web): add /ogretmen/[slug] page

--- apps/web/src/app/ogretmen/[slug]/page.tsx
+++ apps/web/src/app/ogretmen/[slug]/page.tsx
@@ -1,18 +1,37 @@
+import { InfoCard, OverallRatingCard } from "@/components/Card";
+import SingleRating from "@/components/SingleRating";
+import TextSwitch from "@/components/TextSwitch";
 import { initializeApollo } from "@/lib/apollo";
+import { ratingsToLetterGrade } from "@/lib/utils";
 import { gql } from "@apollo/client";
 
 async function getTeacher(slug: string) {
   const apolloClient = initializeApollo();
-  const { data } = await apolloClient.query({
+  const { data } = await apolloClient.query<{ teacher: Teacher[] }>({
     query: gql`
-      query {
-        teacher {
+      query SingleTeacher($slug: String) {
+        teacher(slug: $slug) {
           id
           name
           slug
+          ratings {
+            id
+            score
+            comment
+            createdAt
+          }
+          university {
+            name
+          }
+          faculty {
+            name
+          }
         }
       }
     `,
+    variables: {
+      slug,
+    },
   });
 
   return data.teacher[0];
@@ -20,5 +39,98 @@ async function getTeacher(slug: string) {
 
 export default async function Home({ params }: any) {
   const teacher = await getTeacher(params.slug);
-  return <div>{teacher.name}</div>;
+  const teacherName = teacher.name as string;
+
+  console.log(teacher);
+
+  return (
+    <main>
+      <header className="container mx-auto max-w-6xl">
+        <h1>{teacherName}</h1>
+      </header>
+      <section>
+        <header className="container mx-auto max-w-6xl space-y-8 md:flex md:gap-8 md:space-y-0 md:py-16">
+          <InfoCard
+            image={{
+              src: "https://picsum.photos/200",
+              alt: teacherName,
+            }}
+            title={teacherName}
+            description={`${teacherName}, ${teacher.university?.name} üniversitesinde ${teacher.faculty?.name} bölümünde eğitim veriyor.`}
+            footer={<></>}
+          />
+          <OverallRatingCard
+            letterGrade={ratingsToLetterGrade(teacher.ratings)}
+            gradeText={`${teacherName}'nin toplam ${
+              teacher.ratings?.length
+            } değerlendirme sonucu aldığı harf notu ${ratingsToLetterGrade(
+              teacher.ratings
+            )} olarak hesaplanmıştır.`}
+            scores={[
+              {
+                category: "Eğitim",
+                info: "Kurumun genel öğretim kadrosu ve verilen eğitim kalitesi",
+                max: 100,
+                value: 65,
+              },
+              {
+                category: "Prestij",
+                info: "Kurumun genel itibarını, bilinirliğini, tanınırlığını ve referans anlamındaki değerini",
+                max: 100,
+                value: 100,
+              },
+              {
+                category: "İmkanlar",
+                info: "Öğrenciye sağlanan ayrıcalıklar, indirimler, yardımlar veya staj potansiyelleri",
+                max: 100,
+                value: 77,
+              },
+              {
+                category: "Altyapı",
+                info: "Kurumun ısınma, havalandırma, yaz ve kız şartlarına uygun olarak aldığı aksiyonları, internet ve teknolojiye uygunluğu",
+                max: 100,
+                value: 24,
+              },
+              {
+                category: "Güvenlik",
+                info: "Kurumun genel güvenliği, güvenlik ekibi ve güvenlik protokolleri",
+                max: 100,
+                value: 10,
+              },
+            ]}
+          />
+        </header>
+        <div>
+          <h2 className="my-8 w-full bg-black py-8 text-center text-2xl font-bold text-white md:text-4xl">
+            <TextSwitch
+              links={[
+                {
+                  href: `/ogretmen/${teacher.slug}`,
+                  label: "Değerlendirmeler",
+                },
+              ]}
+            />
+          </h2>
+        </div>
+        <div className="container mx-auto max-w-6xl">
+          <main>
+            <div className="space-y-4 md:flex md:gap-4">
+              <div className="space-y-4 sm:w-full md:max-w-3xl">
+                {teacher.ratings && teacher.ratings.length > 0 ? (
+                  teacher.ratings.map((rating) => (
+                    <SingleRating key={rating.id} {...rating} />
+                  ))
+                ) : (
+                  <p className="text-2xl font-medium">
+                    <span className="font-bold">{teacherName}</span> için henüz
+                    değerlendirme yapılmamış.
+                  </p>
+                )}
+              </div>
+            </div>
+          </main>
+        </div>
+      </section>
+    </main>
+  );
 }
