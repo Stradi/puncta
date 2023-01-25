699c5da7b7449f676fef575eb9a0003e3e205b78
Batın Evirgen <batinevirgen@gmail.com>
2023-01-25 17:21:16 +0300

    feat(web): create RateInput component

---
+++ apps/web/src/components/RateForm/RateInput.tsx
@@ -0,0 +1,90 @@
+import { useField } from "formik";
+import { AnimatePresence, motion } from "framer-motion";
+import { useMemo, useState } from "react";
+import RateInputStyles from "./RateInput.module.css";
+
+interface RateInputProps extends React.ComponentPropsWithoutRef<"input"> {
+  name: string;
+  label: string;
+
+  valueToText: {
+    [key: number]: string;
+  };
+}
+
+export default function RateInput({
+  name,
+  label,
+  valueToText,
+  ...props
+}: RateInputProps) {
+  const [field, meta, helpers] = useField({ name });
+  const [isDragging, setIsDragging] = useState(false);
+  const [valueText, setValueText] = useState("");
+
+  useMemo(() => {
+    const closestValue = Object.keys(valueToText).reduce((prev, curr) => {
+      return Math.abs((curr as any) - field.value) <
+        Math.abs((prev as any) - field.value)
+        ? curr
+        : prev;
+    });
+
+    setValueText(valueToText[Number.parseInt(closestValue)]);
+  }, [field.value, valueToText]);
+  return (
+    <div className="relative">
+      <label htmlFor={name} className="font-medium text-neutral-700">
+        {label}
+      </label>
+      <AnimatePresence>
+        {
+          <motion.span
+            key={valueText}
+            className="absolute left-0 top-0 w-full select-none text-center font-medium text-neutral-700"
+            initial={{
+              scale: 0.75,
+              opacity: 0,
+            }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.75, opacity: 0 }}
+            transition={{
+              duration: 0.1,
+            }}
+          >
+            {valueText}
+          </motion.span>
+        }
+      </AnimatePresence>
+      <input
+        {...field}
+        {...props}
+        type="range"
+        className={RateInputStyles.input}
+        onMouseDown={() => setIsDragging(true)}
+        onMouseUp={() => setIsDragging(false)}
+      />
+      <AnimatePresence>
+        {meta.error && meta.touched && (
+          <motion.div
+            className="text-sm font-medium text-red-600"
+            initial={{
+              height: 0,
+              opacity: 0,
+            }}
+            animate={{
+              height: "auto",
+              opacity: 1,
+            }}
+            exit={{
+              height: 0,
+              opacity: 0,
+            }}
+          >
+            {meta.error}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
