bc1b4dc1dc3ad97da5d2c6c491aebf9fcfe6fd94
Batın Evirgen <batinevirgen@gmail.com>
2023-01-26 17:47:31 +0300

    fix(web): add key prop to seperator element in joinReactChildren utility function

--- apps/web/src/lib/utils.tsx
+++ apps/web/src/lib/utils.tsx
@@ -45,5 +45,11 @@ export function joinReactChildren(
   children: React.ReactNode[],
   seperator: React.ReactNode
 ) {
-  return children.reduce((prev, curr) => [prev, seperator, curr]);
+  return children.reduce((prev, curr, idx) => {
+    const keyedSeperator = Object.create({
+      ...(seperator as React.ReactElement),
+      key: `seperator-${idx}`,
+    });
+    return [prev, keyedSeperator, curr];
+  });
 }
