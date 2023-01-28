6bec82ce2ec34bc2a2857061e53d7f9313e1a11d
Batın Evirgen <batinevirgen@gmail.com>
2023-01-28 14:03:55 +0300

    feat(web): show confirmation screen in rating modal for accidental closes

--- apps/web/src/components/RateForm/RateForm.tsx
+++ apps/web/src/components/RateForm/RateForm.tsx
@@ -5,7 +5,7 @@ import { RateContext } from "@/context/RateContext";
 import { FormikProps } from "formik";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/navigation";
-import { createRef, useContext } from "react";
+import { createRef, useContext, useEffect } from "react";
 import Button from "../Button";
 import BaseRatingForm from "./forms/BaseRatingForm";
 import FinalScreen from "./forms/FinalScreen";
@@ -21,6 +21,49 @@ export default function RateForm(props: React.ComponentPropsWithoutRef<"div">) {
   const modalContext = useContext(ModalContext);
 
   const router = useRouter();
+
+  useEffect(() => {
+    if (rateContext.step === 0) {
+      modalContext.setCloseConfirmation(null);
+    } else {
+      modalContext.setCloseConfirmation(
+        <div>
+          <h1 className="mb-4 text-2xl font-medium">
+            Değerlendirmen kaydedilmeyecek.
+          </h1>
+          <p className="mb-4">
+            Eğer bu pencereyi kapatırsanız hali hazırda girdiğin puanlar
+            kaybolacak.
+          </p>
+        </div>
+      );
+    }
+  }, [rateContext.step, modalContext]);
+
+  async function handlePrev() {
+    rateContext.prevStep();
+  }
+
+  async function handleNext() {
+    // If we are in the last step (rateContext.step === 4), we need to
+    // rate the teacher or university.
+    if (rateContext.step === 4) {
+      const isDone = await rateContext.rate();
+      if (!isDone) {
+        // Something happened, show error.
+        return;
+      }
+
+      modalContext.setIsOpen(false);
+      router.refresh();
+    } else {
+      if (rateContext.step === 0) {
+        rateContext.nextStep();
+      } else {
+        formRef.current?.submitForm();
+      }
+    }
+  }
 
   return (
     <div className="py-8 px-2">
@@ -53,32 +96,12 @@ export default function RateForm(props: React.ComponentPropsWithoutRef<"div">) {
         <Button
           variant="text"
           fullWidth
-          onClick={() => {
-            rateContext.prevStep();
-          }}
+          onClick={handlePrev}
           disabled={rateContext.step <= 0}
         >
           Geri Dön
         </Button>
-        <Button
-          fullWidth
-          onClick={async () => {
-            if (rateContext.step === 0) {
-              rateContext.nextStep();
-            } else if (rateContext.step === 4) {
-              const isDone = await rateContext.rate();
-              if (!isDone) {
-                // Something happened, show error.
-                return;
-              }
-
-              modalContext.setIsOpen(false);
-              router.refresh();
-            } else {
-              formRef.current?.submitForm();
-            }
-          }}
-        >
+        <Button fullWidth onClick={handleNext}>
           {rateContext.step === 4 ? "Paylaş" : "Devam Et"}
         </Button>
       </div>
