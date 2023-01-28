675c64c6a9c71e22c81d9431da31ef1223f46d2f
Batın Evirgen <batinevirgen@gmail.com>
2023-01-28 14:02:55 +0300

    feat(web): add close confirmation screen to modal

--- apps/web/src/context/ModalContext.tsx
+++ apps/web/src/context/ModalContext.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import Button from "@/components/Button";
 import Modal from "@/components/Modal";
-import { AnimatePresence } from "framer-motion";
-import { createContext, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { createContext, useState } from "react";
 
 interface ModalContextProps {
   isOpen: boolean;
@@ -10,6 +11,9 @@ interface ModalContextProps {
 
   content: React.ReactNode;
   setContent: (content: React.ReactNode) => void;
+
+  closeConfirmation: React.ReactNode;
+  setCloseConfirmation: (instantClose: React.ReactNode) => void;
 }
 
 export const ModalContext = createContext<ModalContextProps>(
@@ -19,13 +23,68 @@ export const ModalContext = createContext<ModalContextProps>(
 export function ModalProvider({ children }: React.PropsWithChildren<{}>) {
   const [isOpen, setIsOpen] = useState(false);
   const [content, setContent] = useState<React.ReactNode>(null);
+  const [closeConfirmation, setCloseConfirmation] =
+    useState<React.ReactNode>(null);
+
+  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
+
+  function handleClose() {
+    if (!closeConfirmation) {
+      setIsOpen(false);
+    } else {
+      setIsConfirmationOpen(true);
+    }
+  }
 
   return (
-    <ModalContext.Provider value={{ isOpen, setIsOpen, content, setContent }}>
+    <ModalContext.Provider
+      value={{
+        isOpen,
+        setIsOpen,
+        content,
+        setContent,
+        closeConfirmation,
+        setCloseConfirmation,
+      }}
+    >
       {children}
       <AnimatePresence>
-        {isOpen && (
-          <Modal handleClose={() => setIsOpen(false)}>{content}</Modal>
+        {isOpen && <Modal handleClose={handleClose}>{content}</Modal>}
+        {isConfirmationOpen && (
+          <Modal
+            handleClose={() => setIsConfirmationOpen(false)}
+            maxWidth="sm"
+            hideCloseIcon
+          >
+            {closeConfirmation}
+            <div>
+              <AnimatePresence>
+                <motion.div
+                  className="flex"
+                  initial={{ opacity: 0, height: "0px" }}
+                  animate={{ opacity: 1, height: "auto" }}
+                  exit={{ opacity: 0, height: "0px" }}
+                >
+                  <Button
+                    variant="text"
+                    fullWidth
+                    onClick={() => setIsConfirmationOpen(false)}
+                  >
+                    İptal
+                  </Button>
+                  <Button
+                    fullWidth
+                    onClick={() => {
+                      setIsOpen(false);
+                      setIsConfirmationOpen(false);
+                    }}
+                  >
+                    Kapat
+                  </Button>
+                </motion.div>
+              </AnimatePresence>
+            </div>
+          </Modal>
         )}
       </AnimatePresence>
     </ModalContext.Provider>
