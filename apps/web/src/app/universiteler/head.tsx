f1918e3180a595184375aea55811417ddf5fe1d0
Batın Evirgen <batinevirgen@gmail.com>
2023-01-30 21:43:20 +0300

    fix(web): migrate all universities page to pages directory

--- apps/web/src/app/universiteler/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Üniversiteler | Puncta</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Puncta" />
-    </>
-  );
-}
