1f4904cbce684378d22908146fdcdb14ae009a19
Batın Evirgen <batinevirgen@gmail.com>
2023-01-30 21:16:21 +0300

    fix(web): migrate login page to pages directory

--- apps/web/src/app/giris-yap/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Giriş Yap | Puncta</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Puncta" />
-    </>
-  );
-}
