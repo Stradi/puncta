801f1f7d890ed2fa4c08811415ad58116e7cfeee
Batın Evirgen <batinevirgen@gmail.com>
2023-01-31 13:11:18 +0300

    fix(api): resolve university and faculty fields in 'me' query

--- apps/api/src/user/user.resolver.ts
+++ apps/api/src/user/user.resolver.ts
@@ -2,8 +2,12 @@ import { UseGuards } from '@nestjs/common';
 import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { GqlAuthGuard } from 'src/auth/graphql-auth.guard';
 import { UserEntity } from 'src/common/decorators/user.decorator';
+import { GetFacultyArgs } from 'src/faculty/dto/get-faculty.args';
+import { Faculty } from 'src/faculty/entities/faculty.entity';
 import { GetRatingArgs } from 'src/rating/dto/get-rating.args';
 import { Rating } from 'src/rating/entities/rating.entity';
+import { GetUniversityArgs } from 'src/university/dto/get-university.args';
+import { University } from 'src/university/entities/university.entity';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
@@ -21,6 +25,18 @@ export class UserResolver {
   async ratings(@Parent() user: User, @Args() args: GetRatingArgs) {
     const { id } = user;
     return this.userService.ratings(id, args);
+  }
+
+  @ResolveField('university', () => University)
+  async university(@Parent() user: User, @Args() args: GetUniversityArgs) {
+    const { id } = user;
+    return this.userService.university(id, args);
+  }
+
+  @ResolveField('faculty', () => Faculty)
+  async faculty(@Parent() user: User, @Args() args: GetFacultyArgs) {
+    const { id } = user;
+    return this.userService.faculty(id, args);
   }
 
   // TODO: Add updateUser, changePassword mutations.
--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { GetFacultyArgs } from 'src/faculty/dto/get-faculty.args';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { GetRatingArgs } from 'src/rating/dto/get-rating.args';
+import { convertArgsToWhereClause } from 'src/shared/utils/prisma.utils';
+import { GetUniversityArgs } from 'src/university/dto/get-university.args';
 
 @Injectable()
 export class UserService {
@@ -17,6 +20,45 @@ export class UserService {
       include: {
         university: true,
         teacher: true,
+      },
+      take: args.pageSize,
+      skip: args.page * args.pageSize,
+    });
+  }
+
+  async university(id: number, args: GetUniversityArgs) {
+    return await this.prismaService.university.findFirst({
+      where: {
+        users: {
+          some: {
+            id,
+          },
+        },
+        ...convertArgsToWhereClause(['id', 'slug', 'name'], args),
+      },
+      include: {
+        faculties: true,
+        ratings: true,
+        teachers: true,
+      },
+      take: args.pageSize,
+      skip: args.page * args.pageSize,
+    });
+  }
+
+  async faculty(id: number, args: GetFacultyArgs) {
+    return await this.prismaService.faculty.findFirst({
+      where: {
+        users: {
+          some: {
+            id,
+          },
+        },
+        ...convertArgsToWhereClause(['id', 'slug', 'name'], args),
+      },
+      include: {
+        universities: true,
+        teachers: true,
       },
       take: args.pageSize,
       skip: args.page * args.pageSize,
