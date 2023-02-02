735badc1b88d9b0c5af48d58ef636211b051c06f
Batın Evirgen <batinevirgen@gmail.com>
2023-02-02 17:31:35 +0300

    feat(api): add ApprovedTeacherGuard to guard some routes for teachers

---
+++ apps/api/src/common/approved-teacher.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class ApprovedTeacherGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const ctx = GqlExecutionContext.create(context);
+    const { user } = ctx.getContext().req;
+
+    if (!user) {
+      return false;
+    }
+
+    return user.isApproved;
+  }
+}
