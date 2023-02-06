8c0ea0a52dac0ce38ec8719d66b556f48d432e2e
Batın Evirgen <batinevirgen@gmail.com>
2023-02-06 21:23:39 +0300

    fix(web): move RateSlider's current value text to end of the slider

--- apps/web/src/components/RateForm/RateSlider.tsx
+++ apps/web/src/components/RateForm/RateSlider.tsx
@@ -19,7 +19,6 @@ export default function RateSlider({
   ...props
 }: RateSliderProps) {
   const [field, meta, helpers] = useField({ name });
-  const [isDragging, setIsDragging] = useState(false);
   const [valueText, setValueText] = useState("");
 
   useMemo(() => {
@@ -33,37 +32,21 @@ export default function RateSlider({
     setValueText(valueToText[Number.parseInt(closestValue)]);
   }, [field.value, valueToText]);
   return (
-    <div className="relative">
+    <div>
       <label htmlFor={name} className="font-medium text-neutral-700">
         {label}
       </label>
-      <AnimatePresence>
-        {
-          <motion.span
-            key={valueText}
-            className="absolute left-0 top-0 w-full select-none text-center font-medium text-neutral-700"
-            initial={{
-              scale: 0.75,
-              opacity: 0,
-            }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0.75, opacity: 0 }}
-            transition={{
-              duration: 0.1,
-            }}
-          >
-            {valueText}
-          </motion.span>
-        }
-      </AnimatePresence>
-      <input
-        {...field}
-        {...props}
-        type="range"
-        className={RateSliderStyles.input}
-        onMouseDown={() => setIsDragging(true)}
-        onMouseUp={() => setIsDragging(false)}
-      />
+      <div className="flex items-center justify-between gap-2">
+        <input
+          {...field}
+          {...props}
+          type="range"
+          className={RateSliderStyles.input}
+        />
+        <span className="mb-1 h-full w-8 text-center font-mono text-2xl font-bold">
+          {valueText}
+        </span>
+      </div>
       <AnimatePresence>
         {meta.error && meta.touched && (
           <motion.div
