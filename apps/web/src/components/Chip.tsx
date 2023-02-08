bd2c51319dfcf45589306c7d1fdae11f60fac1f2
Batın Evirgen <batinevirgen@gmail.com>
2023-02-08 23:10:24 +0300

    feat(web): add shadow to Chip component

--- apps/web/src/components/Chip.tsx
+++ apps/web/src/components/Chip.tsx
@@ -1,10 +1,18 @@
+import { cn } from "@/lib/utils";
+
 interface ChipProps {
   label: string;
+  shadows?: boolean;
 }
 
-export default function Chip({ label }: ChipProps) {
+export default function Chip({ label, shadows = false }: ChipProps) {
   return (
-    <div className="flex select-none items-center justify-center rounded-full px-4 py-1 text-sm font-medium text-black ring-1 ring-black">
+    <div
+      className={cn(
+        "flex select-none items-center justify-center rounded-full px-4 py-1 text-sm font-medium text-black ring-1 ring-black",
+        shadows && "shadow-[2px_4px_0_0_rgba(0,0,0,1.0)]"
+      )}
+    >
       {label}
     </div>
   );
