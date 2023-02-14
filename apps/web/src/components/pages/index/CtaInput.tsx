43161ccc419d7206e690c04dd7c8878f1fcf2134
Batın Evirgen <batinevirgen@gmail.com>
2023-02-14 17:07:30 +0300

    feat(web): show clickable suggestions in CtaInput under the search box

--- apps/web/src/components/pages/index/CtaInput.tsx
+++ apps/web/src/components/pages/index/CtaInput.tsx
@@ -1,8 +1,11 @@
 import Button from "@/components/Button";
+import { searchTerm } from "@/lib/search";
 import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "framer-motion";
+import debounce from "lodash.debounce";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import * as yup from "yup";
 
 const CtaInputValidationSchema = yup.object().shape({
@@ -25,6 +28,14 @@ export default function CtaInput({
 }: CtaInputProps) {
   const [term, setTerm] = useState("");
   const [error, setError] = useState<string | null>(null);
+  const [suggestions, setSuggestions] = useState<
+    {
+      name: string;
+      slug: string;
+      type: "university" | "teacher";
+    }[]
+  >([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   const router = useRouter();
 
@@ -38,8 +49,40 @@ export default function CtaInput({
       });
   }
 
+  const debouncedChangeHandler = useCallback(debounce(handleChange, 100), []);
+  useEffect(() => {
+    return () => {
+      debouncedChangeHandler.cancel();
+    };
+  }, []);
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setError(null);
+    setTerm(e.target.value);
+  }
+
+  useEffect(() => {
+    async function getSuggestions() {
+      searchTerm(term).then((res) => {
+        setSuggestions(
+          res.results.slice(0, 2).map((item: any) => ({
+            name: item.name,
+            slug: item.slug,
+            type: item.type,
+          }))
+        );
+      });
+    }
+
+    if (term.length > 2) {
+      getSuggestions();
+    } else {
+      setSuggestions([]);
+    }
+  }, [term]);
+
   return (
-    <div className={className}>
+    <div className={cn("relative", className)}>
       <div
         className={cn(
           "group relative flex items-center",
@@ -50,18 +93,25 @@ export default function CtaInput({
       >
         <input
           type="text"
-          value={term}
           placeholder={placeholder}
-          onChange={(e) => {
-            setError(null), setTerm(e.target.value);
-          }}
+          onChange={debouncedChangeHandler}
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               onSearchButtonClick();
             }
           }}
-          onBlur={() => setError(null)}
-          onClick={() => setError(null)}
+          onBlurCapture={(e) => {
+            setError(null);
+            setTimeout(() => {
+              setShowSuggestions(false);
+            }, 75);
+          }}
+          onClick={() => {
+            setError(null), setShowSuggestions(true);
+          }}
+          onFocus={() => {
+            setError(null), setShowSuggestions(true);
+          }}
           className={cn(
             "w-full rounded-full p-4 outline-none sm:px-8",
             "text-base font-medium ring-1  ring-black sm:text-xl",
@@ -76,11 +126,58 @@ export default function CtaInput({
             "transition duration-100"
           )}
         >
-          <Button className="h-full" onClick={onSearchButtonClick}>
+          <Button
+            className="h-full"
+            onClick={onSearchButtonClick}
+            onFocus={() => {
+              setError(null), setShowSuggestions(true);
+            }}
+          >
             Hemen Ara
           </Button>
         </div>
       </div>
+      <AnimatePresence>
+        {showSuggestions && suggestions.length > 0 && (
+          <motion.ul
+            className="absolute mt-2 w-full rounded-3xl border border-black bg-white font-medium"
+            initial={{
+              height: 0,
+              opacity: 0,
+            }}
+            animate={{
+              height: "auto",
+              opacity: 1,
+            }}
+            exit={{
+              height: 0,
+              opacity: 0,
+            }}
+          >
+            <div className="p-2">
+              {suggestions.slice(0, 2).map((item) => {
+                const href =
+                  item.type === "university" ? "/universite" : "/ogretmen";
+
+                return (
+                  <li
+                    key={item.slug}
+                    className="rounded-full hover:ring-1 hover:ring-black"
+                  >
+                    <Link
+                      href={`${href}/${item.slug}`}
+                      className="inline-block w-full py-2 px-4"
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </div>
+          </motion.ul>
+        )}
+      </AnimatePresence>
+
       <AnimatePresence>
         {error && (
           <motion.div
