1633811b9c02bb92bf272216e457d6a446ed1614
Batın Evirgen <batinevirgen@gmail.com>
2023-02-22 14:33:25 +0300

    feat(web): add contact page

---
+++ apps/web/src/pages/iletisim.tsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>İletişim | The Puncta</title>
+      </Head>
+      <main className="container mx-auto max-w-6xl space-y-16 px-2">
+        <header>
+          <h1 className="text-center text-3xl font-bold md:text-6xl">
+            İletişim
+          </h1>
+        </header>
+        <section className="prose prose-neutral prose-lg mx-auto">
+          <p>
+            Her türlü görüş, öneri ve şikayetleriniz için bizimle iletişime
+            geçebilirsiniz. Aşağıda bize doğrudan ulaşabileceğiniz bir e-posta
+            adresi bulunmaktadır. İsteklerinizi, önerilerini veya
+            şikayetlerinizi aşağıdaki e-posta adresine doğrudan
+            gönderebilirsiniz.
+          </p>
+          <div>
+            <p>
+              E-posta adresimiz:{" "}
+              <a href="mailto:legal@thepuncta.com">legal@thepuncta.com</a>
+            </p>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
