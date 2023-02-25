7458fe66af59339446ec60198ca1f89e7ed81418
Batın Evirgen <batinevirgen@gmail.com>
2023-02-25 20:17:07 +0300

    fix(web): make search better by using contains instead of startsWith

--- apps/web/src/lib/search.ts
+++ apps/web/src/lib/search.ts
@@ -1,10 +1,10 @@
-import { gql } from "@apollo/client";
-import { default as _slugify } from "slugify";
-import { initializeApollo } from "./apollo";
+import { gql } from '@apollo/client';
+import { default as _slugify } from 'slugify';
+import { initializeApollo } from './apollo';
 
 const SEARCH_UNIVERSITY_QUERY = gql`
   query SearchUniversity($term: String!) {
-    university(filter: { slug: { startsWith: $term } }) {
+    university(filter: { slug: { contains: $term } }) {
       name
       slug
     }
@@ -13,7 +13,7 @@ const SEARCH_UNIVERSITY_QUERY = gql`
 
 const SEARCH_TEACHER_QUERY = gql`
   query SearchTeacher($term: String!, $pageSize: Int) {
-    teacher(filter: { slug: { startsWith: $term } }, pageSize: $pageSize) {
+    teacher(filter: { slug: { contains: $term } }, pageSize: $pageSize) {
       name
       slug
       university {
@@ -42,7 +42,7 @@ export async function searchUniversity(term: string) {
   });
 
   if (response.errors && response.errors.length > 0) {
-    console.log("Something happened while registering.");
+    console.log('Something happened while registering.');
     console.log(response.errors);
     return null;
   }
@@ -67,7 +67,7 @@ export async function searchTeacher(term: string, pageSize: number = 12) {
   });
 
   if (response.errors && response.errors.length > 0) {
-    console.log("Something happened while registering.");
+    console.log('Something happened while registering.');
     console.log(response.errors);
     return null;
   }
