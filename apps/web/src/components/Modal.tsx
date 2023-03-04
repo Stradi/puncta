707c60fd63ca354b0b75dd1cc8e25327a536b161
Batın Evirgen <batinevirgen@gmail.com>
2023-03-04 16:37:24 +0300

    fix(web): center modal regardless of scroll position

--- apps/web/src/components/Modal.tsx
+++ apps/web/src/components/Modal.tsx
@@ -1,48 +1,26 @@
-import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
-import { CloseIcon } from "./Icons";
+import { cn } from '@/lib/utils';
+import { motion } from 'framer-motion';
+import { CloseIcon } from './Icons';
 
-interface ModalProps extends React.ComponentPropsWithoutRef<"div"> {
+interface ModalProps extends React.ComponentPropsWithoutRef<'div'> {
   handleClose: () => void;
-  maxWidth?: "sm" | "md" | "lg";
+  maxWidth?: 'sm' | 'md' | 'lg';
   hideCloseIcon?: boolean;
 }
 
-export default function Modal({
-  handleClose,
-  maxWidth = "md",
-  hideCloseIcon = false,
-  children,
-  className,
-}: ModalProps) {
+export default function Modal({ handleClose, maxWidth = 'md', hideCloseIcon = false, children, className }: ModalProps) {
   return (
-    <motion.div
-      className={cn("absolute inset-0 h-full w-full", "bg-black/50")}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      onClick={handleClose}
-    >
+    <motion.div className={cn('fixed inset-0 h-full w-full', 'bg-black/50')} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} onClick={handleClose}>
       <div
-        className={cn(
-          "relative h-full md:top-1/2 md:mx-auto md:h-auto md:-translate-y-1/2",
-          "border-2 border-black bg-white",
-          {
-            "max-w-md": maxWidth === "sm",
-            "max-w-lg": maxWidth === "md",
-            "max-w-xl": maxWidth === "lg",
-          }
-        )}
+        className={cn('relative h-full md:top-1/2 md:mx-auto md:h-auto md:-translate-y-1/2', 'border-2 border-black bg-white', {
+          'max-w-md': maxWidth === 'sm',
+          'max-w-lg': maxWidth === 'md',
+          'max-w-xl': maxWidth === 'lg',
+        })}
         onClick={(e) => e.stopPropagation()}
       >
-        <div className={cn("p-4", className)}>
-          {!hideCloseIcon && (
-            <CloseIcon
-              size="lg"
-              onClick={handleClose}
-              className="float-right cursor-pointer"
-            />
-          )}
+        <div className={cn('p-4', className)}>
+          {!hideCloseIcon && <CloseIcon size="lg" onClick={handleClose} className="float-right cursor-pointer" />}
           <div>{children}</div>
         </div>
       </div>
