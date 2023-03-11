f71d7d31bd13fff69e0eef925541b667af2c8ea1
Batın Evirgen <batinevirgen@gmail.com>
2023-03-11 13:18:12 +0300

    fix(web): update search page

--- apps/web/src/lib/search.ts
+++ apps/web/src/lib/search.ts
@@ -26,6 +26,24 @@ const SEARCH_TEACHER_QUERY = gql`
   }
 `;
 
+const GET_TEACHERS_BY_UNIVERSITY_FACULTY_QUERY = gql`
+  query GetTeacherByUniversityFaculty(
+    $universityName: String!
+    $facultyName: String!
+  ) {
+    teacher(
+      filter: {
+        university: { name: { equals: $universityName } }
+        faculty: { name: { equals: $facultyName } }
+      }
+      pageSize: 100
+    ) {
+      name
+      slug
+    }
+  }
+`;
+
 function slugify(text: string) {
   return _slugify(text, { lower: true });
 }
@@ -61,6 +79,31 @@ export async function searchTeacher(term: string, pageSize: number = 12) {
     variables: {
       term: slugified,
       pageSize,
+    },
+  });
+
+  if (response.errors && response.errors.length > 0) {
+    return null;
+  }
+
+  if (!response.data || !response.data.teacher) {
+    return null;
+  }
+
+  return response.data.teacher;
+}
+
+export async function getTeachersOfUniversityFaculty(
+  universityName: string,
+  facultyName: string
+) {
+  const apolloClient = initializeApollo();
+
+  const response = await apolloClient.query({
+    query: GET_TEACHERS_BY_UNIVERSITY_FACULTY_QUERY,
+    variables: {
+      universityName,
+      facultyName,
     },
   });
 
